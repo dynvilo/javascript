ead157d85a26d2a4e847aac13487f68d1d8833d2
dynvilo <dynfagot@gmail.com>
2025-03-28 22:25:28 +0000

    clase1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Initial commit
+
+# Estudiar
+
+[W3Schools](https://www.w3schools.com/js/default.asp)
+
+[RoadMap](https://roadmap.sh/javascript)
+
+
---
+++ js/clase1.js
@@ -0,0 +1,21 @@
+// Comentario de una linea
+
+/**
+Comentario multilinea
+Multiples
+*/
+
+/**
+Quiero operaciones basicas
+ */
+
+num1 = 8
+num2 = 9
+suma = num1 + num2
+console.log(suma)
+multiplica = num1 * num2
+console.log(multiplica)
+resta = num1 - num2
+console.log(resta)
+division = num1 / num2
+console.log(division)
